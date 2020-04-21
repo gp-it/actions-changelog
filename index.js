9411b14399e5b5005433a7a925bd1eb57122385d
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2020-04-21 13:08:20 +0200

    test last_tag

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const exec = require('@actions/exec')
 
 
 // most @actions toolkit packages have async methods
@@ -13,6 +14,9 @@ async function run() {
     core.debug((new Date()).toTimeString())
 
     core.setOutput('time', new Date().toTimeString());
+
+    const last_tag = await exec.exec('git tag')
+    console.log(last_tag)
   }
   catch (error) {
     core.setFailed(error.message);
