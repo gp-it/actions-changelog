612e1b9e8eabeee700e2683be860f0dc68103981
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2020-04-21 13:41:28 +0200

    test last_tag

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+// const wait = require('./wait');
 const exec = require('@actions/exec')
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-    console.log('test')
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    // const ms = core.getInput('milliseconds');
+    // console.log(`Waiting ${ms} milliseconds ...`)
+    // console.log('test')
+    // core.debug((new Date()).toTimeString())
+    // await wait(parseInt(ms));
+    // core.debug((new Date()).toTimeString())
 
-    core.setOutput('time', new Date().toTimeString());
+    // core.setOutput('time', new Date().toTimeString());
 
     const last_tag = await exec.exec('git tag')
     console.log(last_tag)
