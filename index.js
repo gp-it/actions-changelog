08b946de54939eb22597e5b0b09ed1295e7df8d1
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2020-04-21 12:58:23 +0200

    petit test

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const wait = require('./wait');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
+  try {
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`)
-
+    console.log('test')
     core.debug((new Date()).toTimeString())
     await wait(parseInt(ms));
     core.debug((new Date()).toTimeString())
 
     core.setOutput('time', new Date().toTimeString());
-  } 
+  }
   catch (error) {
     core.setFailed(error.message);
   }
