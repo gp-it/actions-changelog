2b59fdd64d2b38f69807d2bb9b9fe1a22d378de6
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2020-05-05 10:48:08 +0200

    Transformation en script npm

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+#!/usr/bin/env node
+
+'use strict';
+
 const exec = require('child_process').exec
 const fs = require('fs')
 
 exec('git describe --abbrev=0 --tags',
-  (error, stdout, stderr) => {
+  (error, stdout) => {
     if (stdout) {
       exec(`git log ${stdout.trim()}..master --oneline`,
-        (error, stdout, stderr) => {
+        (error, stdout) => {
           if (stdout) {
             const stream = fs.createWriteStream('Changelog.md', { flags: 'w' })
             stream.write(stdout)
-            exec('git add .')
-            exec('git commit -a -m "update changelog"')
           }
         })
     }
